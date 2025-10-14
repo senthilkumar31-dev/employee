a62c691dc845a08fca7474f488fce7a7f7e1752e
Anju M <anju.m@theeminence.org>
2025-10-14 17:44:48 +0530

    Updated the test script

---
+++ hello2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A simple script with variables
+name="ShellScript"
+age=29
+echo "Hello, my name is $name and I am $age years old."
---
+++ hello3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A simple script with variables
+name="TesterMan"
+age=21
+echo "Hello, my name is $name and I am $age years old."
